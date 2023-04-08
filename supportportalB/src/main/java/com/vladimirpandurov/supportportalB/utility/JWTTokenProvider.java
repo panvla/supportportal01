4336e360c1b7f9e102d8eba7b55ce8e27c1d97e7
panvla <pandvlado@gmail.com>
2023-04-08 19:13:32 +0200

    added jwt filter handler..

--- supportportalB/src/main/java/com/vladimirpandurov/supportportalB/utility/JWTTokenProvider.java
+++ supportportalB/src/main/java/com/vladimirpandurov/supportportalB/utility/JWTTokenProvider.java
@@ -1,18 +1,35 @@
 package com.vladimirpandurov.supportportalB.utility;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.vladimirpandurov.supportportalB.constant.SecurityConstant;
 import com.vladimirpandurov.supportportalB.domain.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import static com.vladimirpandurov.supportportalB.constant.SecurityConstant.AUTHORITIES;
+import static com.vladimirpandurov.supportportalB.constant.SecurityConstant.TOKEN_CANNOT_BE_VERIFIED;
+import static java.util.Arrays.stream;
+
+@Component
 public class JWTTokenProvider {
+
     @Value("${jwt.secret}")
     private String secret;
 
@@ -21,14 +38,32 @@ public class JWTTokenProvider {
         return JWT.create().withIssuer(SecurityConstant.V_P_LLC)
                 .withAudience(SecurityConstant.V_P_LLC)
                 .withIssuedAt(new Date()).withSubject(userPrincipal.getUsername())
-                .withArrayClaim(SecurityConstant.AUTHORITIES,claims)
+                .withArrayClaim(AUTHORITIES,claims)
                 .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstant.EXPIRATION_TIME))
                 .sign(Algorithm.HMAC512(secret.getBytes()));
     }
 
-    //public List<GrantedAuthority> getAuthorities(String token){
+    public List<GrantedAuthority> getAuthorities(String token){
+        String[] claims  = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
 
-    //}
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier(){
+        JWTVerifier verifier;
+        try{
+            Algorithm alforithm = Algorithm.HMAC512(secret);
+            verifier = JWT.require(alforithm).withIssuer(SecurityConstant.V_P_LLC).build();
+        }catch (JWTVerificationException exception){
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
+    }
 
     private String[] getClaimsFromUser(UserPrincipal userPrincipal){
         List<String> authorities = new ArrayList<>();
@@ -36,6 +71,28 @@ public class JWTTokenProvider {
             authorities.add(grantedAuthority.getAuthority());
         }
         return authorities.toArray(new String[0]);
+    }
+
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request){
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new
+                UsernamePasswordAuthenticationToken(username, null, authorities);
+        usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return usernamePasswordAuthenticationToken;
+    }
+
+    public boolean isTokenValid(String username, String token){
+        JWTVerifier verifier = getJWTVerifier();
+        return StringUtils.isNotEmpty(username)&& !isTokenExpired(verifier, token);
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
+    }
+
+    public String getSubject(String token){
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getSubject();
     }
 
 }
